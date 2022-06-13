d4f860d30d9ba596cd981e6df4a492ff09186ec7
Charles Jarvis <csjarvis43@gmail.com>
2022-06-13 09:46:41 -0600

    commit tree 6bcb8665544e6153c1df09ee1ab1aeba8204ee14

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+// const newUser = {
+//     name: 'Charlie Jarvis',
+//     email: 'charlie@example.com'
+// }
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email
+        }) 
+    })
+    
+    .then(res => res.json())
+    .then(data => document.body.innerHTML = data["id"])
+
+    .catch(error => document.body.innerHTML = error.message)
+}
+
+
+submitData()
